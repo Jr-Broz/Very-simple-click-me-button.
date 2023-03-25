4fe32b1a07331f89f7d313ee0fd67f6bc5f8f1d1
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-03-25 03:10:59 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function clicar(){
+    var name = prompt("What is your name?");
+  return(alert("nice to meet you", name ));
+
+};
+
+
+
+/*function changeColor(newColor) {
+  const elem = document.getElementById("para");
+  elem.style.color = newColor;
+} */
